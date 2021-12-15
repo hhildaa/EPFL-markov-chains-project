c208fdaf6f9a8634d8479c30a32e17b663034034
Nina-Mainusch <Nina.Mainusch@protonmail.com>
2021-12-15 18:28:13 +0100

    Feat: Houdayer phase transition plots

--- phase_plots.py
+++ phase_plots.py
@@ -12,7 +12,7 @@ def theoretical_phase_transition(a, b):
     return r_crit
 
 
-def plot_phase_transition_metropolis(x, ys, ns, r_crits):
+def plot_phase_transition_metropolis(x, ys, ns, r_crits, algorithm):
     colors = ["blue", "orange", "green"]
     plt.figure()
     for idx in range(len(ns)):
@@ -22,25 +22,48 @@ def plot_phase_transition_metropolis(x, ys, ns, r_crits):
     plt.xlabel("b/a")
     plt.ylabel("limit of the empirical overlap q_n")
     plt.legend()
+    plt.savefig(f"plots/{algorithm}_phase_transition.jpg")
     plt.show()
 
 
-if __name__ == '__main__':
+def metropolis_phase_viz(r_crits, ns):
     # Metropolis
+    # it_nums = [5000, 5000, 6000, 6000, 6000, 6000, 6000, 1000, 1000]
     metropolis_100 = load_pickle("data/", "algo_metropolis_N_100.pickle")
+    metropolis_500 = load_pickle("data/", "algo_metropolis_N_500.pickle")
     metropolis_1000 = load_pickle("data/", "algo_metropolis_N_1000.pickle")
 
     y_metropolis_100 = metropolis_100['avg_estimation_overlap']
-    r_crit_metropolis_100 = theoretical_phase_transition(40, 60)
-
-    y_metropolis_500 = [1, 1, 1, 1, 1, 1, 1, 0, 0]
-    r_crit_metropolis_500 = theoretical_phase_transition(200, 300)
-
+    y_metropolis_500 = metropolis_500['avg_estimation_overlap']
     y_metropolis_1000 = metropolis_1000['avg_estimation_overlap']
-    r_crit_metropolis_1000 = theoretical_phase_transition(400, 600)
 
-    r_crits_metropolis = [r_crit_metropolis_100, r_crit_metropolis_500, r_crit_metropolis_1000]
     ys_metropolis = [y_metropolis_100, y_metropolis_500, y_metropolis_1000]
-    ns_metropolis = [100, 500, 1000]
 
-    plot_phase_transition_metropolis(metropolis_100['b_div_a'], ys_metropolis, ns_metropolis, r_crits_metropolis)
+    plot_phase_transition_metropolis(metropolis_100['b_div_a'], ys_metropolis, ns, r_crits, algorithm="metropolis")
+
+
+def houdayer_viz(r_crits, ns):
+    # it_nums = [500, 500, 500, 500, 500, 500, 500, 500, 500]
+    houdayer_100 = load_pickle("data/", "algo_houdayer_N_100_n0_2.pickle")
+    houdayer_500 = load_pickle("data/", "algo_houdayer_N_500.pickle")
+    houdayer_1000 = load_pickle("data/", "algo_houdayer_N_1000.pickle")
+
+    y_houdayer_100 = houdayer_100['avg_estimation_overlap']
+    y_houdayer_500 = houdayer_500['avg_estimation_overlap']
+    y_houdayer_1000 = houdayer_1000['avg_estimation_overlap']
+
+    ys_metropolis = [y_houdayer_100, y_houdayer_500, y_houdayer_1000]
+
+    plot_phase_transition_metropolis(houdayer_100['b_div_a'], ys_metropolis, ns, r_crits, algorithm="houdayer_n0_2")
+
+
+if __name__ == '__main__':
+    r_crit_100 = theoretical_phase_transition(40, 60)
+    r_crit_500 = theoretical_phase_transition(200, 300)
+    r_crit_1000 = theoretical_phase_transition(400, 600)
+    r_critical = [r_crit_100, r_crit_500, r_crit_1000]
+    n_s = [100, 500, 1000]
+
+    houdayer_viz(r_critical, n_s)
+    metropolis_phase_viz(r_critical, n_s)
+
--- phase_transitions.py
+++ phase_transitions.py
@@ -30,56 +30,83 @@ import pickle
 import numpy as np
 
 from graph import generate_x_hat, generate_x, generate_adjacency_matrix
-from metropolis import metropolis
+from metropolis import metropolis, houdayer
 from phase_plots import theoretical_phase_transition
+from utils import save_pickle, DATAPATH
 
 DATA_PATH = './data/'
 
 if __name__ == '__main__':
+    a_list = None
+    b_list = None
+    n_0 = None
+    algorithm = 'houdayer'
+    rounds = 10
 
-    a_list = np.array([70, 60, 57, 55, 54, 53, 52, 51, 50]) * 10
-    b_list = np.array([30, 40, 43, 45, 46, 47, 48, 49, 50]) * 10
-    it_nums = [4000, 4000, 6000, 6000, 6000, 6000, 6000, 1000, 1000]
+    for N in [100, 500, 1000]:
 
-    N = 1000
+        if N == 100:
+            a_list = np.array([70, 60, 57, 55, 54, 53, 52, 51, 50])
+            b_list = np.array([30, 40, 43, 45, 46, 47, 48, 49, 50])
+        elif N == 500:
+            a_list = np.array([70, 60, 57, 55, 54, 53, 52, 51, 50]) * 5
+            b_list = np.array([30, 40, 43, 45, 46, 47, 48, 49, 50]) * 5
+        elif N == 1000:
+            a_list = np.array([70, 60, 57, 55, 54, 53, 52, 51, 50]) * 10
+            b_list = np.array([30, 40, 43, 45, 46, 47, 48, 49, 50]) * 10
 
-    assert len(a_list) == len(b_list) == len(it_nums)
-    assert [a_list[i]+b_list[i] for i in range(len(a_list))] == [N]*len(a_list)
+        it_nums = [500, 500, 500, 500, 500, 500, 500, 500, 500]
 
-    beta = 1
-    algo = 'metropolis'
-    avg_estimation_overlaps = []
-    rounds = 100
+        assert len(a_list) == len(b_list) == len(it_nums)
+        assert [a_list[i]+b_list[i] for i in range(len(a_list))] == [N]*len(a_list)
 
-    r_crit = theoretical_phase_transition(a_list[0], b_list[0])
-    print(f'Critical phase transition expected at r_crit = {round(r_crit, 4)}.')
+        beta = 1
+        avg_estimation_overlaps = []
 
-    for idx in range(0, len(a_list)):
-        a = a_list[idx]
-        b = b_list[idx]
-        it_num = it_nums[idx]
-        print(f'Current a = {a} Current b = {b}')
-        print(f'Current r = {round(b/a, 4)}.')
+        r_crit = theoretical_phase_transition(a_list[0], b_list[0])
+        print(f'Critical phase transition expected at r_crit = {round(r_crit, 4)}.')
 
-        x_star = generate_x(N, 5)
-        adj_matrix = generate_adjacency_matrix(x_star, a, b, N)
+        for idx in range(0, len(a_list)):
+            a = a_list[idx]
+            b = b_list[idx]
+            it_num = it_nums[idx]
+            print(f'Current a = {a} Current b = {b}')
+            print(f'Current r = {round(b/a, 4)}.')
 
-        estimation_overlap_over_runs = []
-        for experiment_run in range(rounds):
-            x_hat_1 = generate_x_hat(N)
+            x_star = generate_x(N, 5)
+            adj_matrix = generate_adjacency_matrix(x_star, a, b, N)
 
-            _, estimation_overlaps = \
-                metropolis(x_hat_1, adj_matrix, a, b, N, beta, x_star, it_num=it_num)
+            estimation_overlap_over_runs = []
+            estimation_overlap_over_runs_2 = []
 
-            estimation_overlap_over_runs.append(estimation_overlaps[-1])
+            for experiment_run in range(rounds):
+                x_hat_1 = generate_x_hat(N)
+                if algorithm == "metropolis":
+                    n_0 = 0
+                    _, estimation_overlaps = \
+                        metropolis(x_hat_1, adj_matrix, a, b, N, beta, x_star, it_num=it_num)
 
-            print(f'Iteration: {experiment_run + 1}/ {rounds} \t overlap: {round(estimation_overlaps[-1], 4)}')
+                elif algorithm == "houdayer":
+                    n_0 = 2  # Normal houdayer
+                    x_hat_2 = generate_x_hat(N)
+                    estimation_overlaps, estimation_overlaps_2 = \
+                        houdayer(x_hat_1, x_hat_2, adj_matrix, a, b, N, beta, x_star, n_0=n_0, it_num=it_num)
+                    estimation_overlap_over_runs_2.append(estimation_overlaps_2[-1])
 
-        print(round(sum(estimation_overlap_over_runs) / len(estimation_overlap_over_runs), 4))
-        avg_estimation_overlaps.append(round(sum(estimation_overlap_over_runs) / len(estimation_overlap_over_runs), 4))
+                estimation_overlap_over_runs.append(estimation_overlaps[-1])
 
-    b_div_a = np.array(b_list) / np.array(a_list)
-    save_dict = {'b_div_a': b_div_a, 'N': N, 'algo': algo, 'avg_estimation_overlap': avg_estimation_overlaps}
+                print(f'Iteration: {experiment_run + 1}/ {rounds} \t overlap: {round(estimation_overlaps[-1], 4)}')
 
-    with open(DATA_PATH + f'algo_{algo}_N_{N}' + '.pickle', 'wb') as handle:
-        pickle.dump(save_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            print(round(sum(estimation_overlap_over_runs) / len(estimation_overlap_over_runs), 4))
+            if algorithm == "metropolis":
+                avg_estimation_overlaps.append(round(sum(estimation_overlap_over_runs)
+                                                     / len(estimation_overlap_over_runs), 4))
+            elif algorithm == "houdayer":
+                chain_1 = sum(estimation_overlap_over_runs) / len(estimation_overlap_over_runs)
+                chain_2 = sum(estimation_overlap_over_runs_2) / len(estimation_overlap_over_runs_2)
+                avg_estimation_overlaps.append(round(np.mean([chain_1, chain_2]), 4))
+
+        b_div_a = np.array(b_list) / np.array(a_list)
+        save_dict = {'b_div_a': b_div_a, 'N': N, 'algo': algorithm, 'avg_estimation_overlap': avg_estimation_overlaps}
+
+        save_pickle(save_dict, DATAPATH, f'algo_{algorithm}_N_{N}_n0_{n_0}' + '.pickle')
